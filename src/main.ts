47e0d4474da1bc2a5b4ade8a857cdf97404c17be
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 06:17:39 -0700

    nestjs server setup

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // setup helmet
+  app.use(helmet());
+
+  // setup global http exception filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // setup validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
+  // setup cors
+  app.enableCors();
+
+  if (process.env.NODE_ENV !== 'production') {
+    // setup swagger
+    const config = new DocumentBuilder()
+      .setTitle('Kenkeputa API')
+      .setDescription('Kenkeputa API description')
+      .setVersion('1.0')
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          name: 'JWT',
+          description: 'Enter JWT token',
+          in: 'header',
+        },
+        'JWT-auth',
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1/doc', app, document);
+  }
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
