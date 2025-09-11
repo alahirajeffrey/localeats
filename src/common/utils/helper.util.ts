5c1873e1d477c3c8ea230d6311aa71a56c836994
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 06:45:50 -0700

    added helper function to hash coordinates

---
+++ src/common/utils/helper.util.ts
@@ -0,0 +1,8 @@
+import geohash from './ngeohash';
+
+export const geohashCoordinates = (
+  longitude: number,
+  latitude: number,
+): string => {
+  return geohash.encode(longitude, latitude, 9) as string;
+};
