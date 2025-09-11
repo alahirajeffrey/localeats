a5d682522a0a5222537b471bfed328abf98a586f
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 08:46:49 -0700

    added restaurant controllers

--- src/modules/restaurant/restaurant.controller.ts
+++ src/modules/restaurant/restaurant.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { RestaurantService } from './restaurant.service';
+import { CreateRestaurantDto, UpdateRestaurantDto } from './restaurant.dto';
+import { ApiResponse } from 'src/common';
 
-@Controller('restaurant')
-export class RestaurantController {}
+@Controller('restaurants')
+export class RestaurantController {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
+  @Get(':restaurantId')
+  async getRestaurantById(
+    @Param('restaurantId') restaurantId: string,
+  ): Promise<ApiResponse> {
+    return this.restaurantService.fetchRestaurantById(restaurantId);
+  }
+
+  @Post()
+  async createRestaurant(
+    @Body() dto: CreateRestaurantDto,
+  ): Promise<ApiResponse> {
+    return this.restaurantService.addRestaurant(dto);
+  }
+
+  @Delete(':restaurantId')
+  async removeRestaurant(
+    @Param('restaurantId') restaurantId: string,
+  ): Promise<ApiResponse> {
+    return this.restaurantService.deleteRestaurant(restaurantId);
+  }
+
+  @Put(':restaurantId')
+  async updateRestaurant(
+    @Param('restaurantId') restaurantId: string,
+    @Body() dto: UpdateRestaurantDto,
+  ): Promise<ApiResponse> {
+    return this.restaurantService.updateRestaurantDetails(restaurantId, dto);
+  }
+
+  /*
+  @Get()
+  async findRestaurants(
+    @Query() dto: FindRestaurantDto,
+  ): Promise<ApiResponse> {
+    return this.restaurantService.findRestaurants(dto);
+  }
+  */
+}
