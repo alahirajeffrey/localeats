b2c578cf88e37e5a8d5ba69ebac317df2efea424
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 06:18:21 -0700

    file exports

---
+++ src/common/config/index.ts
@@ -0,0 +1,8 @@
+import { EnvConfig } from '../interfaces';
+
+export const envConfig: EnvConfig = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_PORT: Number(process.env.PORT) || 3000,
+  DATABASE_URL: process.env.DATABASE_URL || '',
+  REDIS_URL: process.env.REDIS_URL || '',
+};
