6456c063a6d77c653e2b3fa19d61dd209ff27968
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 10:40:35 -0700

    added documentation to helper functions

--- src/common/utils/helper.util.ts
+++ src/common/utils/helper.util.ts
@@ -1,13 +1,27 @@
 import geohash from 'ngeohash';
 import haversine from 'haversine-distance';
 
+/**
+ * Hash coordinates into a geohash string
+ * @param latitude Latitude of the location
+ * @param longitude Longitude of the location
+ * @returns string representing geohash
+ */
 export const geohashCoordinates = (
-  longitude: number,
   latitude: number,
+  longitude: number,
 ): string => {
-  return geohash.encode(longitude, latitude, 9) as string;
+  return geohash.encode(latitude, longitude, 9) as string;
 };
 
+/**
+ * Check if a restaurant is within a given distance from user
+ * @param longitude User longitude
+ * @param latitude User latitude
+ * @param restaurantGeohash Restaurant geohash
+ * @param distanceInMetres Maximum allowed distance in meters
+ * @returns boolean
+ */
 export const isProximityMatch = (
   longitude: number,
   latitude: number,
