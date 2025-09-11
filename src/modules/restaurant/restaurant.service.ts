4e46010b354ac81060cb90899f137e0bec49e3c7
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 06:21:20 -0700

    commit

---
+++ src/modules/restaurant/restaurant.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RestaurantService {
+  constructor() {}
+
+  async findRestaurants() {}
+
+  async addRestaurant() {}
+
+  async deleteRestaurant() {}
+}
