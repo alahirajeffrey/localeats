809be7d0fe48aadd94319f86a0cadbad99d95220
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 08:46:20 -0700

    commit

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -2,7 +2,7 @@ import { EnvConfig } from '../interfaces';
 
 export const envConfig: EnvConfig = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  NODE_PORT: Number(process.env.PORT) || 3000,
+  NODE_PORT: Number(process.env.NODE_PORT) || 3000,
   DATABASE_URL: process.env.DATABASE_URL || '',
   REDIS_URL: process.env.REDIS_URL || '',
 };
