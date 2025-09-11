dbc168c4e0dc57f0733bdc8e42121f4c5e1e6329
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 10:14:28 -0700

    added helper function to check proximity of locations

--- src/common/utils/helper.util.ts
+++ src/common/utils/helper.util.ts
@@ -1,8 +1,28 @@
 import geohash from 'ngeohash';
+import haversine from 'haversine-distance';
 
 export const geohashCoordinates = (
   longitude: number,
   latitude: number,
 ): string => {
   return geohash.encode(longitude, latitude, 9) as string;
+};
+
+export const isProximityMatch = (
+  longitude: number,
+  latitude: number,
+  restaurantGeohash: string,
+  distanceInMetres: number,
+): boolean => {
+  const [decodedLat, decodedLon] = geohash.decode(restaurantGeohash) as [
+    number,
+    number,
+  ];
+
+  const userLocation = { lat: latitude, lon: longitude };
+  const restaurantLocation = { lat: decodedLat, lon: decodedLon };
+
+  const dist = haversine(userLocation, restaurantLocation);
+
+  return dist <= distanceInMetres;
 };
