f33c47a9fe81714afe01a768b5815a8c4d287216
alahirajeffrey <alahirajeffrey@gmail.com>
2025-09-11 08:43:25 -0700

    setup restaurant module

--- src/modules/restaurant/restaurant.module.ts
+++ src/modules/restaurant/restaurant.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RestaurantController } from './restaurant.controller';
 import { RestaurantService } from './restaurant.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [RestaurantController],
-  providers: [RestaurantService]
+  providers: [RestaurantService, PrismaService],
 })
 export class RestaurantModule {}
